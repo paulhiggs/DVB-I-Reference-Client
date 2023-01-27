96a5ddb2f9ae0ef1d650ca35aed453dda8da20e7
Paul Higgs <paul_higgs@hotmail.com>
2023-01-27 23:49:52 +0000

    dont show non-LCN services

--- frontend/configuration.js
+++ frontend/configuration.js
@@ -1,2 +1,5 @@
 // DVB-I Reference installation location -- also backend/configuration.php
 var INSTALL_LOCATION="http://119.8.215.119/DemoHouse";
+
+// set to true to include <Service> channels that are not included in the selected LCN table.
+var INCLUDE_NON_LCN_CHANNELS=false;
